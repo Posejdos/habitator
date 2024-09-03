fe577b86d910654546f2efe151f914a1f40a0d19
Marcin Gawron <marcin@Marcins-MacBook-Air.local>
2024-09-03 19:16:48 +0200

    Mark habit as failed

--- app/main.py
+++ app/main.py
@@ -99,6 +99,43 @@ def mark_done(username, habit_name):
         click.echo(f"Habit '{habit_name}' not found for user: {username}")
 
 
+# Mark failed a habit for user
+
+
+@click.command()
+@click.option(
+    "--username",
+    prompt="Enter your username",
+    help="The username of the user.",
+)
+@click.option(
+    "--habit-name",
+    prompt="Enter the name of the habit to mark as failed",
+    help="The name of the habit to mark as failed.",
+)
+def mark_failed(username, habit_name):
+    user = User(username, f"tests/{username}.json")
+    user_found = user.try_load_data_from_json()
+
+    if not user_found:
+        click.echo(f"User not found: {username}")
+        return
+
+    habit_found = False
+    for habit in user.habits:
+        if habit.name == habit_name:
+            habit.fail()
+            habit_found = True
+            break
+
+    if habit_found:
+        user.save_data_to_json()
+        click.echo(f"Habit '{habit_name}' marked as failed for user: {username}")
+    else:
+        click.echo(f"Habit '{habit_name}' not found for user: {username}")
+
+
+cli.add_command(mark_failed)
 cli.add_command(mark_done)
 cli.add_command(add_habit)
 cli.add_command(read)
