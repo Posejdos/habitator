9c1035216d48768d78fd2163a29ad2211fa128e4
Marcin Gawron <marcin@Marcins-MacBook-Air.local>
2024-09-03 18:58:12 +0200

    Make it work on MacOS. Also, move init.py to main.py

--- app/main.py
+++ app/main.py
@@ -1,12 +1,18 @@
 import click
 from user import User
+from habit import Habit
+
 
 @click.group()
 def cli():
     pass
 
+
+# Read Habits of user
+
+
 @click.command()
-@click.option('--username', default='Steve', help='The username of the person.')
+@click.option("--username", default="Steve", help="The username of the person.")
 def read(username):
     user = User(username, f"tests/{username}.json")
     user.try_load_data_from_json()
@@ -18,6 +24,7 @@ def read(username):
     click.echo(f"Habits for user: {username} \n")
     for habit in user.habits:
         click.echo(habit)
+
 
 cli.add_command(read)
 if __name__ == "__main__":
